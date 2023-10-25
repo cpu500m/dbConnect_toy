9c964f8da5902bd25fbb71b865b998bd5006eec6
cpu500m <paul108203@naver.com>
2023-10-25 20:07:49 +0900

    로그인 -완-

--- src/main/java/toy1/upload_toy/UploadToyApplication.java
+++ src/main/java/toy1/upload_toy/UploadToyApplication.java
@@ -8,13 +8,13 @@ import toy1.upload_toy.repository.MemberRepository;
 @SpringBootApplication
 public class UploadToyApplication {
 
-	/* TODO
+	/*
 	* -  redirectURL 처리하기				--- O
 	* - 로그인 페이지 , 회원가입 페이지 만들기			--- O
 	* - 글 작성할 때 작성자명 로그인한 회원 id로 자동 기입되도록 수정하기 --- O
 	* - 회원가입 관련 bean validation 적용하기 ---	O
 	* - 로그인 할 때 로그인 실패 시 object error 띄우기. ---	O
-	* - directory 구조 개선 및 코드 리팩터링 ( 다 하고 )
+	* - directory 구조 개선 및 코드 리팩터링 ( 다 하고 ) --- hmm....
 	* */
 	public static void main(String[] args) {
 		SpringApplication.run(UploadToyApplication.class, args);
--- src/main/java/toy1/upload_toy/config/TestDataConfig.java
+++ src/main/java/toy1/upload_toy/config/TestDataConfig.java
@@ -4,19 +4,33 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+import toy1.upload_toy.domain.Item;
 import toy1.upload_toy.domain.Member;
+import toy1.upload_toy.domain.Post;
+import toy1.upload_toy.repository.ItemRepository;
 import toy1.upload_toy.repository.MemberRepository;
+import toy1.upload_toy.repository.PostRepository;
 
 @Component
 @RequiredArgsConstructor
 public class TestDataConfig {
     private final MemberRepository memberRepository;
+    private final PostRepository postRepository;
+    private final ItemRepository itemRepository;
+
     /**
      * 테스트용 유저 등록
      */
     @PostConstruct
     public void init() {
+        // 테스트 유저
         memberRepository.save(Member.createMember("test111", "test111","테스트1"));
         memberRepository.save(Member.createMember("test222", "test222","테스트2"));
+
+        // 포스트
+        postRepository.save(Post.createPost("안녕하세요!", "테스터1"));
+        postRepository.save(Post.createPost("안녕!", "테스터2"));
+        itemRepository.save(Item.createItem("안녕하세요!", "테스터1", "안녕하세요 반갑습니다!", null, null));
+        itemRepository.save(Item.createItem("안녕!", "테스터2", "안녕 반가워~~~!", null, null));
     }
 }
